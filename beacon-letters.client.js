4fdda424c69bf8f8e4962b11e13acef54e7b6ce1
SteveCollie <steve.collie@finbourne.com>
2018-06-13 17:38:12 +0100

    Minor UI update

--- beacon-letters.client.js
+++ beacon-letters.client.js
@@ -2,19 +2,24 @@
 // Listens for the radio signal, plots it on the display and records
 // the letters that the beacon is transmitting
 
+let letters = ""
 let beacons: number[] = []
-let letters: string = ""
-radio.onDataPacketReceived( ({ receivedString, signal, serial: serial2 }) =>  {
-    if (SignalDisplay.showSignal(signal)) {
-    	if(beacons.indexOf(serial2) < 0)
-        {
+let showRadio = false
+radio.onDataPacketReceived(({ receivedString, signal, serial: serial2 }) => {
+    if (showRadio && SignalDisplay.showSignal(signal)) {
+        if (beacons.indexOf(serial2) < 0) {
             beacons.push(serial2)
-            letters = letters.concat(receivedString)
+            letters = "" + letters + receivedString
         }
     }
 })
 input.onButtonPressed(Button.B, () => {
+    showRadio = false
     basic.clearScreen()
     basic.showNumber(beacons.length)
     basic.showString(letters)
+    showRadio = true
 })
+showRadio = true
+radio.setGroup(1)
+
